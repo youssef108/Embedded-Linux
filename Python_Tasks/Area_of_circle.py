0f2ca9328856cdf42f3e35acb3f25ba66651fa3e
Youssef Abdelshafy <youssef.waleed.hassan@gmail.com>
2023-08-13 23:25:43 +0300

    Calander task done

---
+++ Python_Tasks/Area_of_circle.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+radius=int(input("Enter radius: "))
+area= np.pi*radius**2
+print("area: ",area)
---
+++ Python_Tasks/Calander_of_a_month.py
@@ -0,0 +1,5 @@
+import calendar
+year=int(input("input year: "))
+month=int(input("input month: "))
+cal = calendar.month(year, month)
+print(cal)
