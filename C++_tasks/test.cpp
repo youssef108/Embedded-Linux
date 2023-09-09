32457126a566d6a3968ce410d1b055b4deac1da5
Youssef Abdelshafy <youssef.waleed.hassan@gmail.com>
2023-09-10 00:17:15 +0300

    session issues corrected

---
+++ C++_tasks/test.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+int main(){
+    std::cout<<"helloooo"<<std::endl;
+}
--- Python_Tasks/num4counter-Vowelchecker.py
+++ Python_Tasks/num4counter-Vowelchecker.py
@@ -1,14 +1,12 @@
 
 def Vowel_checker(letter):
-    if(letter=='a'or letter=='e' or letter=='i' or letter== 'u '):
+    vowels={'a','e','i','u'}
+    if(letter in vowels):
         return True
     return False
 
 def number_4_counter(lis):
-    count=0
-    for num in lis:
-        if(num==4):
-            count+=1
+    count=lis.count(4)
     print("number of 4s = ",count)
 
 lis=[3,4,6,7,4,2,3,4,6,4,2,5]
